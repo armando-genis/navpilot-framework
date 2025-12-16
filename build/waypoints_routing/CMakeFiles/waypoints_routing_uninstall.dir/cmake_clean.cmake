afc21f391a3fc0f6903f65474c1ac0db7306e015
Armando Genis <armando_genis@outlook.com>
2025-12-16 14:29:37 -0600

    fixed repo

--- build/waypoints_routing/CMakeFiles/waypoints_routing_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/waypoints_routing_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/waypoints_routing_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
