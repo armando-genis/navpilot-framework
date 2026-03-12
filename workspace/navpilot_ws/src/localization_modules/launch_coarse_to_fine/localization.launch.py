53600dcfe8511cfa339ce970f24c8e11bbcd7997
Armando Genis <armando_genis@outlook.com>
2026-03-12 12:57:03 -0600

    coarse to fine localization added

---
+++ workspace/navpilot_ws/src/localization_modules/launch_coarse_to_fine/localization.launch.py
@@ -0,0 +1,116 @@
+import os
+
+import launch
+import launch.actions
+import launch.events
+
+import launch_ros
+import launch_ros.actions
+import launch_ros.events
+
+from launch import LaunchDescription
+from launch_ros.actions import LifecycleNode
+from launch_ros.actions import Node
+
+import lifecycle_msgs.msg
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    ld = launch.LaunchDescription()
+
+    localization_param_dir = os.path.join(current_dir, 'localization.yaml')
+    rviz_param_dir = os.path.join(current_dir, 'localization.rviz')
+    params_rotation_ = os.path.join(current_dir, 'rotation_params.yaml')
+
+
+    publisher_node_rotation = launch_ros.actions.LifecycleNode(
+        package='pointcloud_rotation',
+        namespace='',
+        executable='pointcloud_rotation_node',
+        name='pointcloud_rotation_node',
+        output='screen',
+        parameters=[params_rotation_],
+        additional_env={'RCUTILS_CONSOLE_OUTPUT_FORMAT': "{message}"}
+    )
+
+    lidar_localization = launch_ros.actions.LifecycleNode(
+        name='coarse_to_fine_localization',
+        namespace='',
+        package='coarse_to_fine_localization',
+        executable='coarse_to_fine_localization_node',
+        parameters=[localization_param_dir],
+        output='screen')
+    
+
+    to_inactive = launch.actions.EmitEvent(
+        event=launch_ros.events.lifecycle.ChangeState(
+            lifecycle_node_matcher=launch.events.matches_action(lidar_localization),
+            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+    from_unconfigured_to_inactive = launch.actions.RegisterEventHandler(
+        launch_ros.event_handlers.OnStateTransition(
+            target_lifecycle_node=lidar_localization,
+            goal_state='unconfigured',
+            entities=[
+                launch.actions.LogInfo(msg="-- Unconfigured --"),
+                launch.actions.EmitEvent(event=launch_ros.events.lifecycle.ChangeState(
+                    lifecycle_node_matcher=launch.events.matches_action(lidar_localization),
+                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+                )),
+            ],
+        )
+    )
+
+    from_inactive_to_active = launch.actions.RegisterEventHandler(
+        launch_ros.event_handlers.OnStateTransition(
+            target_lifecycle_node=lidar_localization,
+            start_state = 'configuring',
+            goal_state='inactive',
+            entities=[
+                launch.actions.LogInfo(msg="-- Inactive --"),
+                launch.actions.EmitEvent(event=launch_ros.events.lifecycle.ChangeState(
+                    lifecycle_node_matcher=launch.events.matches_action(lidar_localization),
+                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+                )),
+            ],
+        )
+    )
+
+    robot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('robot_description'),
+                'launch',
+                'sdv_localization.launch.py'
+            ])
+        ]),
+    )
+
+
+    rviz = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_param_dir]
+    )
+
+    ld.add_action(robot_launch)
+    ld.add_action(from_unconfigured_to_inactive)
+    ld.add_action(from_inactive_to_active)
+    ld.add_action(publisher_node_rotation)
+    ld.add_action(lidar_localization)
+    ld.add_action(to_inactive)
+    ld.add_action(rviz)
+
+    return ld
--- workspace/navpilot_ws/test.md
+++ workspace/navpilot_ws/test.md
@@ -8,4 +8,7 @@ colcon build --packages-select pointcloud_clustering
 colcon build --packages-select pointcloud_rotation
 colcon build --packages-select pointcloud_clustering_KDTree
 
+colcon build --packages-select coarse_to_fine_localization
+
 ros2 launch /workspace/navpilot_ws/src/localization_modules/launch/localization.launch.py
+ros2 launch /workspace/navpilot_ws/src/localization_modules/launch_coarse_to_fine/localization.launch.py
